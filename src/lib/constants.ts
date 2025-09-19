d6bb83eb362bb932173c97a598ee269dcbb6a838
Luis Uchubanda <luisuf@gmail.com>
2025-09-19 03:43:52 +0000

    INSTAGRAM POST INSTAGRAM HISTORIAS HAZLOS AL 50% MAS PEQUENOS

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const BANNER_PRESETS = {
   facebookCover: { name: 'Facebook Cover', width: 851, height: 315 },
-  instagramPost: { name: 'Instagram Post', width: 1080, height: 1080 },
-  instagramStory: { name: 'Instagram Story', width: 1080, height: 1920 },
+  instagramPost: { name: 'Instagram Post', width: 540, height: 540 },
+  instagramStory: { name: 'Instagram Story', width: 540, height: 960 },
   twitterHeader: { name: 'Twitter Header', width: 1500, height: 500 },
   linkedinBanner: { name: 'LinkedIn Banner', width: 1584, height: 396 },
   youtubeChannel: { name: 'YouTube Channel Art', width: 2560, height: 1440 },
