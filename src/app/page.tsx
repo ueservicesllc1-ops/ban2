55e50472aaae997e9a6192252480a67e5c951a8e
Luis Uchubanda <luisuf@gmail.com>
2025-09-19 04:17:14 +0000

    ahora ponle un marco a toda la pagina haz todo mas pequeno que no ocupe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import { withAuth } from '@/components/with-auth';
 
 function HomePage() {
   return (
-    <div className="flex flex-col h-dvh bg-background overflow-hidden">
-      <Header />
-      <main className="flex-1 min-h-0">
-        <BannerEditor />
-      </main>
+    <div className="p-4 lg:p-8 h-dvh">
+      <div className="flex flex-col h-full rounded-lg shadow-lg overflow-hidden bg-background ring-1 ring-border">
+        <Header />
+        <main className="flex-1 min-h-0">
+          <BannerEditor />
+        </main>
+      </div>
     </div>
   );
 }
